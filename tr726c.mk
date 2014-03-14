a2043eff279861aaadef214a6af0607ef25656a7
RuanJG <rjg@t-chip.com.cn>
2014-03-14 11:38:42 +0800

    Tchip: 添加需求配置文件，在TCTools/mkimage.sh里会根据这个文件进行修改系统属性

--- tr726c.mk
+++ tr726c.mk
@@ -37,8 +37,3 @@ TARGET_ROCKCHIP_PCBATEST := true
 #  Tchip hideapk: hideapk.txt
 PRODUCT_COPY_FILES += device/rockchip/$(PRODUCT_NAME)/cusmter/hideapk.txt:system/usr/data/hideapk.txt
 
-# launcher ui
-PRODUCT_COPY_FILES += device/rockchip/$(PRODUCT_NAME)/cusmter/launcher/appwidgets.xml:system/usr/data/appwidgets.xml
-PRODUCT_COPY_FILES += device/rockchip/$(PRODUCT_NAME)/cusmter/launcher/com.android.launcher3.prefs.xml:system/usr/data/com.android.launcher3.prefs.xml
-PRODUCT_COPY_FILES += device/rockchip/$(PRODUCT_NAME)/cusmter/launcher/launcher.db:system/usr/data/launcher.db
-PRODUCT_COPY_FILES += device/rockchip/$(PRODUCT_NAME)/cusmter/launcher/launcher.preferences:system/usr/data/launcher.preferences
